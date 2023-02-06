b10ca5d05cc035c29f12e53682e42b6dfce87ae6
unknown <emprenderusinque@hotmail.com>
2023-02-06 15:49:28 -0500

    UUID - identificador unico

--- src/todos/models/todo.model.js
+++ src/todos/models/todo.model.js
@@ -1,3 +1,6 @@
+import { v4 as uuid } from "uuid";
+
+
 export class Todo {
     /** 
      * 
@@ -6,7 +9,7 @@ export class Todo {
 
 
     constructor( description ) {
-        this.id = 1;
+        this.id = uuid();
         this.description = description
         this.done = false;
         this.createdAt = new Date();
