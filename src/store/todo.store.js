dd0ea10504eb3c704728c16c729235c16837ba7f
unknown <emprenderusinque@hotmail.com>
2023-02-07 17:38:50 -0500

    implementar metodos del store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -25,13 +25,31 @@ const loadStore = () => {
     throw new Error('Not implemented');
 }
 
+const getTodos = ( filter = Filters.All ) => {
+    switch ( filter ) {
+        case Filters.All :
+            return [...state.todos]
+        
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done ); 
+        
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done);
+        default:
+            throw new Error(`Option ${ filter } is not valid`)
+    }
+}
+
 /**
  * 
  * @param { String } description 
  */
 
+
 const addTodo = ( description ) => {
-    throw new Error('Not implemented');
+    if( !description ) throw new Error('Not implemented');
+
+    state.todos.push( new Todo(description));
 };
 
 const toggleTodo = (todoId) => {
@@ -39,19 +57,24 @@ const toggleTodo = (todoId) => {
 };
 
 const deleteTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.id !== todoId)
 };
 
 const deleteCompleted = () => {
-    throw new Error('Not implemented');
+    state.todos = state.todos.filter( todo => todo.done )
 };
 
+/**
+ * 
+ * @param {* Filters } newFilter 
+ */
+
 const setFilter = ( newFilter =  Filters.All ) => {
-    throw new Error('Not implemented');
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Not implemented');
+    return state.filter;
 }
 
 export default {
@@ -62,5 +85,6 @@ export default {
     deleteTodo,
     deleteCompleted,
     setFilter,
-     getCurrentFilter
+    getCurrentFilter,
+    getTodos
 } 
